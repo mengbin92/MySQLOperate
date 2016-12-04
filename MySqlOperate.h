41a12d58d3319082d64ee063032118dfadf5e1fe
root <root@localhost.localdomain>
2016-12-04 03:10:47 +0000

    nothing

---
+++ MySqlOperate.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <mysql/mysql.h>
+
+using namespace std;
+
+class MysqlOperate
+{
+public:
+    MysqlOperate();
+    ~MysqlOperate();
+};
