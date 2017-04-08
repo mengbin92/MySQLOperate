ded2673c3c1f7bc18f684b104aa3a464243e0f17
root <root@localhost.localdomain>
2017-04-08 09:36:26 +0000

    add func

--- inc/mysqlOpe.h
+++ inc/mysqlOpe.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iomanip>
 
 #include <unistd.h>
 #include <stdio.h>
@@ -13,31 +15,45 @@ using namespace std;
 class mysqlOpe
 {
 public:
-    mysqlOpe();
-    ~mysqlOpe();
+	mysqlOpe();
+	~mysqlOpe();
 
-    mysqlOpe(string host,string user,string passwd,string db,unsigned port = 3306);
-    
-    bool connectDB();
-    void showDatabases();
-    void showTables();
-    void select(string table);
+	mysqlOpe(string host, string user, string passwd, string db, unsigned port = 3306);
+ 
+    //连接数据库
+	bool connectDB();
+	//修改连接的数据库
+    bool changeDB(string dbname);
+    //查看数据库
+	void showDatabases();
+    //查看数据库中的表
+	void showTables();
+    //查看表结构
+	void showTable(string table);
+    //查询数据表中的数据
+	void select(string table);
+	void select(string table, string filename);
 
-    vector<string> getDatabases();
-    vector<string> getTables();
+    //执行sql操作
+    bool querySQL(string sqlCMD);
+
+    //获取数据库
+	vector<string> getDatabases();
+    //获取表
+	vector<string> getTables();
 
 public:
-    MYSQL connect;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
-    MYSQL_FIELD* fields;
-    string sql;
-    int ret;
-    
-    string m_host;
-    string m_user;
-    string m_passwd;
-    string m_dbname;
-    int m_port;
+	MYSQL connect;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	MYSQL_FIELD* fields;
+	string sql;
+	int ret;
+
+	string m_host;
+	string m_user;
+	string m_passwd;
+	string m_dbname;
+	int m_port;
 
 };
--- src/test.cpp
+++ src/test.cpp
@@ -6,35 +6,50 @@ using namespace std;
 int main(int argc,char* argv[])
 {
     vector<string> tables;
+	vector<string> databases;
 
-    mysqlOpe test("104.224.165.127","root",",.klio89","mydb",3306);
-    test.connectDB();
-    /*
-    cout<<"show databases"<<endl;
-    test.showDatabases();
+	mysqlOpe test("104.224.165.127", "root", ",.klio89", "mydb", 3306);
+	test.connectDB();
 
-    cout<<"show tables"<<endl;
-    test.showTables();
+#if 0
+	cout<<"show databases"<<endl;
+	test.showDatabases();
 
-    test.select("emp");
-    */
+	databases = test.getDatabases();
+	for (size_t i = 0; i < databases.size(); ++i)
+	{
+		if (test.changeDB(databases[i]))
+		{
+			tables.clear();
 
-    tables = test.getTables();
-    
-    for(size_t i = 0;i<tables.size();++i)
-    {
-        cout<<"show table "<<tables[i]<<" info..."<<endl;
-    }
+			tables = test.getTables();
+			for (size_t i = 0; i<tables.size(); ++i)
+			{
+				cout << "show table " << tables[i] << " info..." << endl;
+				//test.select(tables[i]);
+				string filename = tables[i] + ".txt";
+				test.select(tables[i], filename);
+				cout << "---------------------" << endl;
+			}
+		}
+	}
+	/*//展示表中的信息
+	tables = test.getTables();
 
-    //c++ 11
-    /*
-    auto iter = tables.begin();
-    while(iter != tables.end())
-    {
-        cout<<"show table "<<*iter<<" info..."<<endl;
-        test.select(*iter);
-        ++iter;
-    }
-    */
+	//c++ 11
+	auto iter = tables.begin();
+	while(iter != tables.end())
+	{
+	cout<<"show table "<<*iter<<" info..."<<endl;
+	test.select(*iter);
+	++iter;
+	}
+	*/
+#endif	
+
+#if 1
+	test.showTable("dept");
+
+#endif
     return 0;
 }
